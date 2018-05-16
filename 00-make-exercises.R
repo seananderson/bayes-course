490e57d7ce4f843446bd1740b6697fd46ddbc3d6
Sean Anderson <sean@seananderson.ca>
2018-05-16 07:07:48 -0400

    Add make

---
+++ 00-make-exercises.R
@@ -0,0 +1,10 @@
+files <- list.files(".", pattern = "*.Rmd")
+dir.create("original-files", showWarnings = FALSE)
+remove_exercises <- function(x) {
+  file.copy(x, "original-files")
+  f <- readLines(x)
+  f_ex <- ifelse(grepl("# exercise", f), "# exercise", f)
+  f_ex <- ifelse(grepl("<!-- exercise -->", f_ex), "<!-- exercise -->", f_ex)
+  writeLines(as.character(f_ex), con = x)
+}
+purrr::walk(files, remove_exercises)
