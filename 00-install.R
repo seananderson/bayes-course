717bc34324ce3eb7f641062d486a74e5a7b5c31a
Sean Anderson <sean@seananderson.ca>
2018-05-17 11:50:26 -0400

    devtools

--- 00-install.R
+++ 00-install.R
@@ -28,7 +28,7 @@ please_install <- function(pkgs, install_fun = install.packages) {
 pkgs <- c(
   "tidyverse", "rstan", "rstanarm", "brms", "rmarkdown",
   "manipulate", "shiny", "usethis", "bayesplot", "loo",
-  "remotes"
+  "devtools"
 )
 have <- rownames(installed.packages())
 needed <- setdiff(pkgs, have)
