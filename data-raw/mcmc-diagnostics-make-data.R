ae28e7078dd0975f59cdf9c1364e653331be974a
Sean Anderson <sean@seananderson.ca>
2025-01-29 11:02:59 -0800

    MCMC diagnostics slides

--- data-raw/mcmc-diagnostics-make-data.R
+++ data-raw/mcmc-diagnostics-make-data.R
@@ -1,3 +1,5 @@
+dir.create("slides/figs", showWarnings = FALSE)
+
 # examples of just MCMC chains to detect issues:
 # - a chain is drifting but OK otherwise
 # - chains are in separate places
@@ -145,3 +147,114 @@ rstan::ess_bulk(samples_matrix)
 rstan::ess_tail(samples_matrix)
 
 saveRDS(samples, "data/mcmc6.rds")
+
+# slides:
+
+library(bayesplot)
+color_scheme_set("viridisD")
+
+# ideal 1 chain
+set.seed(1)
+s1 <- function(i, n = 1000, mu = 0) {
+  x <- arima.sim(n = n, list(ar = 0.01), sd = 1) + mu
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x
+}
+samples <- purrr::map(1, s1)
+names(samples) <- paste("Chain", 1)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-ideal.png", width = 6, height = 4)
+
+# getting stuck in multiple modes
+samples <- purrr::map(1, s1, n = 300, mu = -2)
+samples2 <- purrr::map(1, s1, n = 300, mu = 1)
+samples3 <- purrr::map(1, s1, n = 300, mu = -2)
+samples <- list(rbind(samples[[1]], samples2[[1]], samples3[[1]]))
+names(samples) <- paste("Chain", 1)
+samples_matrix <- do.call(rbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-modes.png", width = 6, height = 4)
+
+# drifting
+set.seed(1)
+s1 <- function(i) {
+  N <- 1000
+  x <- arima.sim(n = N, list(ar = 0.5), sd = 0.3)
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x <- x + 1:1000 * 0.001
+  x
+}
+
+samples <- purrr::map(1, s1)
+names(samples) <- paste("Chain", 1:1)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-drift.png", width = 6, height = 4)
+
+# major autocorrelation
+set.seed(1)
+s1 <- function(i) {
+  N <- 1000
+  x <- arima.sim(n = N, list(ar = 0.97), sd = 0.3)
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x
+}
+
+samples <- purrr::map(1, s1)
+names(samples) <- paste("Chain", 1:1)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-auto.png", width = 6, height = 4)
+
+
+# drifting different ways
+set.seed(1)
+s1 <- function(i, drift = 0.001) {
+  N <- 1000
+  x <- arima.sim(n = N, list(ar = 0.5), sd = 0.3)
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x <- x + (1:1000 - 500) * drift
+  x
+}
+
+samples <- purrr::map2(1:2, c(0.0015, -0.0015), \(i, drift) s1(i, drift))
+names(samples) <- paste("Chain", 1:2)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-drift2.png", width = 6, height = 4)
+
+# stuck in different places
+set.seed(1)
+s1 <- function(i, mu = 0) {
+  N <- 1000
+  x <- arima.sim(n = N, list(ar = 0.5), sd = 0.4) + mu
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x
+}
+
+samples <- purrr::map2(1:2, c(1, -1), \(i, mu) s1(i, mu))
+names(samples) <- paste("Chain", 1:2)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+
+ggsave("slides/figs/chains-different-mean.png", width = 6, height = 4)
+
+# ideal 4 chain
+set.seed(1)
+s1 <- function(i, n = 1000, mu = 0) {
+  x <- arima.sim(n = n, list(ar = 0.1), sd = 1) + mu
+  x <- matrix(x, ncol = 1)
+  colnames(x) <- "theta"
+  x
+}
+samples <- purrr::map(1:4, s1)
+names(samples) <- paste("Chain", 1:4)
+samples_matrix <- do.call(cbind, samples)
+bayesplot::mcmc_trace(samples)
+ggsave("slides/figs/chains-ideal4.png", width = 6, height = 4)
